2cf671cdba4f1dcf2eb460426ac4691c4e554a26
MarcosHeick <marcos.heick@gmail.com>
2023-01-11 21:46:56 -0300

    Finalisimo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './components/Home/home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DaTa from './components/Data/data';
-
+import { Error404 } from './Error404/Error404';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
  
       <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='*' element={<div>NOOOOO</div>}/>
+      <Route path='*' element={<Error404/>}/>
       <Route path='/data' element={<DaTa></DaTa>}/>
       </Routes>
       
